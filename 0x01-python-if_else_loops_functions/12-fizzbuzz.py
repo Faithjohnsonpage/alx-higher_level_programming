2a0a8887403b2aa32ef3bc3c83c5d2dbb95305c1
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-11-29 17:32:59 +0100

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        word1 = "Fizz"
+        word2 = "Buzz"
+        word3 = "FizzBuzz"
+
+        for i in range(1, 101):
+            if i % 3 == 0 and i % 5 == 0:
+                print("{}".format(word3), end=" ")
+            elif i % 3 == 0:
+                print("{}".format(word1), end=" ")
+            elif i % 5 == 0:
+                print("{}".format(word2), end=" ")
+            else:
+                print("{}".format(i), end=" ")
