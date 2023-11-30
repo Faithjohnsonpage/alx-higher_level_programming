169a1fb7fd34d876f406b2b89a5a901ce0e7c117
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-11-30 22:13:08 +0100

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-# Check if there are any arguments
-if len(sys.argv) == 1:
-    print("0 arguments.")
-else:
-    # Print the number of arguments
-    if len(sys.argv) == 2:
-        print("{:d} argument:".format(len(sys.argv) -1))
+    # Check if there are any arguments
+    if len(sys.argv) == 1:
+        print("0 arguments.")
     else:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
+        # Print the number of arguments
+        if len(sys.argv) == 2:
+            print("{:d} argument:".format(len(sys.argv) -1))
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
 
-    # Print each argument
-    for index, argument in enumerate(sys.argv[1:], start=1):
-        print("{:d}: {:s}".format(index, argument))
+        # Print each argument
+        for index, argument in enumerate(sys.argv[1:], start=1):
+            print("{:d}: {:s}".format(index, argument))
