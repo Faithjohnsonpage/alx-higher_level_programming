d56da597a2e8650b226dfab12092743dca5988cb
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-04 00:38:41 +0100

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        new_list[idx] = element
+        return new_list
