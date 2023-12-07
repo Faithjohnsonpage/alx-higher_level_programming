14141cda261269104828a474b385a504f40ee4bd
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-07 17:22:46 +0100

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,14 +2,12 @@
 def weight_average(my_list=[]):
     sum1 = 0
     sum2 = 0
-    frequency_sum = 0
 
     if my_list:
         for i in my_list:
             weight, frequency = i
             sum1 += weight * frequency
             sum2 += frequency
-            frequency_sum += frequency
 
         result = sum1 / sum2 if sum2 != 0 else 0
         return result
