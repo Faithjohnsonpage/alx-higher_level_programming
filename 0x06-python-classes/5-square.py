c3c4aaee42793416e3cd3b91cece86403b1aac7b
Faithjohnsonpage <ermac_4life@yahoo.com>
2023-12-20 21:48:52 +0100

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -56,8 +56,9 @@ class Square:
 
         if self.__size == 0:
             print()
-        else:
-            for i in range(1, self.__size + 1):
-                for j in range(1, self.__size + 1):
-                    print("#", end="\n" if j == self.__size else "")
-                print()
+        for i in range(1, self.__size + 1):
+            for j in range(1, self.__size + 1):
+                print("#", end="\n" if j == self.__size else "")
+            print()
+
+
