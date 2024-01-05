891d6cdf002f3af00d92d483f1ac3d59ee1f9ada
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-01-05 01:46:10 +0100

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A mode that defines a Rectangle class"""
+
+
+class Rectangle:
+    """A Rectangle class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+* Python - More Classes and Objects
+
+These are the solutions to tasks More Classes and Objects - 0x08-python-more_classes
