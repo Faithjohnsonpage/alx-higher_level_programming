0d778c9939174529685551063fcbf7af9094d359
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-01-09 16:01:18 +0100

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that that writes a string to a text file (UTF8)
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Returns the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
