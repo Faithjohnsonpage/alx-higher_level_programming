6f3716be2e6df27df3c9702ba67e0a96266094fd
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-01-16 04:03:10 +0100

    task done1

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,3 @@
+# Python - Almost a circle
+
+* These are the solutions to tasks on projects: Python - Almost a circle
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+A module that defines a Rectangle class
+"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A Rectangle class that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Setter for width"""
+        self.__width = width
+
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Setter for height"""
+        self.__height = height
+
+    @property
+    def x(self):
+        """Getter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """Setter for x"""
+        self.__x = x
+
+    @property
+    def y(self):
+        """Getter for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """Setter for y"""
+        self.__y = y        
