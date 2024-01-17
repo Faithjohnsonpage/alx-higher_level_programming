17eecea4e1d57cc6ac9d55361bcae8bbfc26ee00
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-01-17 19:37:11 +0100

    taskdone12

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -99,7 +99,7 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """Assigns an argument to each attribute"""
-        attributes = ['id', 'width', 'height', 'x', 'y']
+        attributes = ['width', 'height', 'x', 'y', 'id']
         for attr, value in zip(attributes, args):
             setattr(self, attr, value)
 
