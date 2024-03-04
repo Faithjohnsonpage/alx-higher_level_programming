a776b7bb7b7353a86f2b446201b468d45f23c521
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-04 18:28:00 +0100

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,12 +1,4 @@
 -- This script creates the MySQL server user user_0d_1 if it doesn't already exist.
 
--- Check if the user exists
-SELECT user FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost' INTO @user_exists;
-
--- If the user does not exist, create it
-IF @user_exists IS NULL THEN
-    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-ELSE
-	GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-END IF;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
