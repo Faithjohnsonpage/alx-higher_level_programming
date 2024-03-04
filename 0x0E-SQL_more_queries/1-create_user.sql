68a56ce5d6ba6a3191eccfab23b24294dbb0ff0c
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-04 17:49:57 +0100

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script that creates the MySQL server user user_0d_1.
+
+CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
