c4559447c8a2c11998060374f413d8b8bb9e2d70
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-04 21:14:10 +0100

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- (in the database hbtn_0d_usa) on our MySQL server.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-   USE DATABASE hbtn_0d_usa;
+   USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	   name VARCHAR(256) NOT NULL
