f336f220020c0ccef90c5d51a6ccadced0fcc537
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-11 22:53:56 +0100

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  let max = parseInt(process.argv[2]);
+  let secondMax = -Infinity;
+  
+  for (let i = 3; i < process.argv.length; i++) {
+    let num = parseInt(process.argv[i]);
+    
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax) {
+      secondMax = num;
+    }
+  }
+
+  console.log(secondMax === -Infinity ? '0' : secondMax);
+}
