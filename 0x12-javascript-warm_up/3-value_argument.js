d1d201b08d4ef3e47e2e929ad90c9435d814adb9
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-11 16:04:18 +0100

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,6 +2,6 @@
 
 if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv[3] === undefined) {
+} else if (process.argv[2] !== undefined) {
   console.log(process.argv[2]);
 }
