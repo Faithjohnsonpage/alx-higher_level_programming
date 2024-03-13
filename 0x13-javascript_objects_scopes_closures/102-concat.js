1ebbf316756273dda9b9c8f54a17608a86931fea
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-13 19:39:11 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,36 +1,22 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-if (process.argv.length === 5) {
-  const fileA = process.argv[2];
-  const fileB = process.argv[3];
-  const fileC = process.argv[4];
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 
-  fs.readFile(fileA, (errA, dataA) => {
-    if (errA) {
-      console.error(errA);
-      return;
-    }
-    fs.readFile(fileB, (errB, dataB) => {
-      if (errB) {
-        console.error(errB);
-        return;
-      }
-      fs.writeFile(fileC, dataA, err => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        fs.appendFile(fileC, dataB, err => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log(`Files ${fileA} and ${fileB} were merged into ${fileC}`);
-        });
-      });
-    });
-  });
-} else {
-  console.log('Usage: ./merge_files.js <fileA> <fileB> <fileC>');
+if (
+  fs.existsSync(fileA) &&
+fs.statSync(fileA).isFile &&
+fs.existsSync(fileB) &&
+fs.statSync(fileB).isFile &&
+fileC !== undefined
+) {
+  const fileAContent = fs.readFileSync(fileA);
+  const fileBContent = fs.readFileSync(fileB);
+  const stream = fs.createWriteStream(fileC);
+
+  stream.write(fileAContent);
+  stream.write(fileBContent);
+  stream.end();
 }
