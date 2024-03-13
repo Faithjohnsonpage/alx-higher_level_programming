d259b3059f04420b6eab8a820aa397be7297d6f0
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-13 17:42:47 +0100

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
 
-let sorted_dict = {};
-for (let key in dict) {
-  let value = dict[key];
-  if (!(value in sorted_dict)) {
-    sorted_dict[value] = [key];
+const sortedDict = {};
+for (const key in dict) {
+  const value = dict[key];
+  if (!(value in sortedDict)) {
+    sortedDict[value] = [key];
   } else {
-    sorted_dict[value].push(key);
+    sortedDict[value].push(key);
   }
 }
 
-console.log(sorted_dict);
+console.log(sortedDict);
