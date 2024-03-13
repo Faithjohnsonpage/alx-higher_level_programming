72d325d389a76ee78d38e8c6fc0f437cc119c1a6
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-13 15:31:22 +0100

    102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 
 exports.addMeMaybe = function (number, theFunction) {
-  let nb = 1;
-  while (number > 0) {
-    nb++;
-    number--;
-  }
-  theFunction(nb);
+  theFunction(++number);
 };
