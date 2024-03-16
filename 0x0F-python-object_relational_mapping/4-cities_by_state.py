1bcce30ea3821be2c1ca1b0c3ad2840875916213
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-03-16 21:26:35 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db="hbtn_0e_4_usa")
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
     sql_query = "SELECT cities.id, cities.name, states.name \
