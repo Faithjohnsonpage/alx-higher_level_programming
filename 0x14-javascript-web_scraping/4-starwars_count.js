5c9fcfc8db4117afc1563197c948a6c573db3637
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-24 21:53:10 +0100

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+const urlId = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+let data = '';
+let count = 0;
+request
+  .get(url)
+  .on('data', (chunk) => {
+    data += chunk;
+  })
+  .on('end', () => {
+    const jsonContent = JSON.parse(data);
+    const results = jsonContent.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === urlId) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  });
