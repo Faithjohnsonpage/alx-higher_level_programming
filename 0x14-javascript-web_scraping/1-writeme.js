b9075a092f48caead632dc4fcd88500c4f482f6f
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-04-24 16:40:34 +0100

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileName = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(fileName, data, 'utf-8', function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
